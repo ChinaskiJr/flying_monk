a6a8391cbc5bb21068463a17a58c9428f8fb3458
ChinaskiJr <robin.colombier@gmail.com>
2018-06-19 16:59:27 +0200

    improve collisions functions

--- public/js/gameMonk.js
+++ public/js/gameMonk.js
@@ -1,6 +1,8 @@
 $(function() {
 	/* Let's store some data */
 	$('body').after('<p class="lastPositionPillar" hidden></p>');
+	$('body').after('<p class="areYouBlocked" hidden></p>');
+	$('body').after('<p class="areYouDead" hidden>0</p>');
 	/* Music loop */
 	var music = document.createElement('audio');
 	music.setAttribute('src', 'sound/music/music.mp3');
@@ -17,7 +19,8 @@ $(function() {
 		$('.skyBackground').animate({left : '-=960'}, 4000, 'linear', function() {
 			$('.skyBackground:first').css('left', 960);
 			$('.skyBackground:nth-child(2)').css('left', 0);
-			skyIsMoving();
+			if ($('.areYouDead').html() !=1)
+				skyIsMoving();
 		});
 	}
 	/* Monk animation */
@@ -61,7 +64,7 @@ $(function() {
 		pillar.css('left', 870);
 		pillar.css('top', pillarY);
 		pillarIsComing(pillar, pillarY, speed);
-		setInterval(function() {helloPillar(pillar);} , 200);
+		setInterval(function() {helloPillar(pillar);} , 100);
 	}
 	/* Display a pillar */
 	function pillarIsComing(pillar, pillarY, speed) {
@@ -81,9 +84,9 @@ $(function() {
 	function pillarsAreQuicker(timeStart) {
 		var timeNow = $.now();
 		var timeSinceTheBeggining = timeNow - timeStart;
-		var pillarsSpeed = 4500 - (timeSinceTheBeggining / 20);
-		if (pillarsSpeed < 1500) {
-			pillarsSpeed = 1500;
+		var pillarsSpeed = 4500 - (timeSinceTheBeggining / 10);
+		if (pillarsSpeed < 1000) {
+			pillarsSpeed = 1000;
 		}
 		return parseInt(pillarsSpeed);
 	}
@@ -91,12 +94,14 @@ $(function() {
 	function pillarsAreNumerous(timeStart) {
 		var timeNow = $.now();
 		var timeSinceTheBeggining = timeNow - timeStart;
-		var pillarsPace = parseInt(3000 - (timeSinceTheBeggining / 25));
-		if (pillarsPace < 450) {
-			pillarsPace = 450;
+		var pillarsPace = parseInt(2000 - (timeSinceTheBeggining / 20));
+		if (pillarsPace < 300) {
+			pillarsPace = 300;
 		}
-		setNewPillar();
-		setTimeout(function() {pillarsAreNumerous(timeStart);}, pillarsPace);
+		if ($('.areYouDead').html() != 1) {
+			setNewPillar();
+			setTimeout(function() {pillarsAreNumerous(timeStart);}, pillarsPace);
+		}
 	}
 
 	/* COLLISION */ 
@@ -106,35 +111,61 @@ $(function() {
 		var pillarY = parseInt(pillar.css('top'));
 		var pillarX = parseInt(pillar.css('left'));
 		// Meet a pillar from the face
-		while (
+		if (
 		monkY < (pillarY + 400)
 		&& monkY > (pillarY - 64)
 		&& monkX < pillarX
-		&& (monkX + 64) > pillarX) {
-			monkX = pillarX - 90;
-			$('.monkContainer').animate({left: monkX}, 200, 'linear');
-		} // Meet a pillar from behind (you fool...)
-		while (
+		&& (monkX + 60) > pillarX) {
+			monkX = pillarX - 60;
+			$('.monkContainer').animate({left: monkX}, 100, 'linear');
+			$('.areYouBlocked').html(1);
+		} else 
+			$('.areYouBlocked').html(0);
+		// Meet a pillar from behind (you fool...)
+		if (
 		monkY < (pillarY + 400)
 		&& monkY > (pillarY - 64)
-		&& monkX < pillarX + 80
+		&& monkX < pillarX + 60
 		&& monkX > pillarX) {
-			monkX = pillarX + 90;
-			$('.monkContainer').animate({left: monkX}, 200, 'linear');
-		}
+			monkX = pillarX + 70;
+			$('.monkContainer').animate({left: monkX}, 100, 'linear');
+			$('.areYouBlocked').html(1);
+		} else 
+			$('.areYouBlocked').html(0);
+
+		// Meet a pillard from the bottom
+		if (
+		monkY >= (pillarY + 400)
+		&& monkY < (pillarY + 450)
+		&& monkX > pillarX - 40
+		&& monkX < (pillarX + 40)) {
+			monkY = 400;
+			$('.monkContainer').animate({top: monkY}, 100, 'linear');
+			$('.areYouBlocked').html(1);
+		} else 
+			$('.areYouBlocked').html(0);
+		// Meet a pillard from the top
+		if (
+		monkY <= pillarY
+		&& monkY + 70 >= pillarY
+		&& monkX > pillarX - 40
+		&& monkX < (pillarX + 40)) {
+			monkY = 300;
+			$('.monkContainer').animate({top: monkY}, 100, 'linear');
+			$('.areYouBlocked').html(1);
+		} else 
+			$('.areYouBlocked').html(0);
 	}
 
 	/* DEFEAT */
-	var isDead = 0;
 	function monkDead() {
 		var monkX = parseInt($('.monkContainer').css('left'));
-		if (monkX < 0 && isDead === 0) {
-			isDead = 1;
-			music.pause();
+		if (monkX < 0 && $('.areYouDead').html() !=1) {
+			$('.areYouDead').html(1);
+			$('.monkContainer').css('display', 'none');
 			var gameOverSound = document.createElement('audio');
 			gameOverSound.setAttribute('src', 'sound/sounds/gameOver.mp3');
 			gameOverSound.play();
-			$('.gameOver').css('display', 'initial');
 		}
 	}
 
@@ -146,31 +177,32 @@ $(function() {
 
 	/* EVENT GESTION */
 	/* Moving the little guy */
-	$(document).on('keydown', function(e) {
+	$(document).on('keydown', moveTheMonk);
+
+	function moveTheMonk(e) {
 		var monkY = parseInt($('.monkContainer').css('top'));
 		var monkX = parseInt($('.monkContainer').css('left'));
-
 		switch (e.which) {
 			// Top
 			case 38:
-				if (monkY > 10)
+				if (monkY > 10 && $('.areYouBlocked').html() != 1)
 					$('.monkContainer').css('top', monkY -= 50);
 			break;
 			// Bottom
 			case 40:
-				if (monkY < 700)
+				if (monkY < 700 && $('.areYouBlocked').html() != 1)
 					$('.monkContainer').css('top', monkY += 50);
 			break;
 			// Left
 			case 37:
-				if (monkX > 8)
+				if (monkX > 8 && $('.areYouBlocked').html() != 1)
 					$('.monkContainer').css('left', monkX -= 50);
 			break;
 			// Right
 			case 39:
-				if (monkX < 890)
+				if (monkX < 800 && $('.areYouBlocked').html() != 1)
 					$('.monkContainer').css('left', monkX += 50);
 			break;
 		}
-	})
+	}
 });
